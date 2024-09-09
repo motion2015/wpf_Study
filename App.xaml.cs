6a9f70e17bce843f35ac97444db81b5cec2f7b4a
motion2015 <motion74@naver.com>
2024-09-10 01:06:11 +0900

    first wpf study

---
+++ App.xaml.cs
@@ -0,0 +1,60 @@
+﻿//using Kabultalk.Services;
+using Kabultalk.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Kabultalk
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Services = ConfigureServices();
+            Startup += App_Startup;
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            var mainView = App.Current.Services.GetService<MainView>()!;
+            mainView.Show();
+        }
+
+        /// <summary>
+        /// Gets the current <see cref="App"/> instance in use
+        /// </summary>
+        public new static App Current => (App)Application.Current;
+
+        /// <summary>
+        /// Gets the <see cref="IServiceProvider"/> instance to resolve application services.
+        /// </summary>
+        public IServiceProvider Services { get; }
+
+        /// <summary>
+        /// Configures the services for the application.
+        /// </summary>
+        private static IServiceProvider ConfigureServices()
+        {
+            // Services
+            var services = new ServiceCollection();
+
+            // Services
+            //services.AddSingleton<ITestService, TestService>();
+            //views
+            services.AddSingleton<MainView>();
+
+            //services.AddSingleton<IFilesService, FilesService>();
+            //services.AddSingleton<ISettingsService, SettingsService>();
+            //services.AddSingleton<IClipboardService, ClipboardService>();
+            //services.AddSingleton<IShareService, ShareService>();
+            //services.AddSingleton<IEmailService, EmailService>();
+         
+            return services.BuildServiceProvider();
+        }
+    }
+
+}
